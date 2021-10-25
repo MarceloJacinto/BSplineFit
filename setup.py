f02768cceb4e5102a55e12ddf565d4a8b16a1184
Marcelo Jacinto <marcelo.jacinto@tecnico.ulisboa.pt>
2021-10-25 15:40:41 +0100

    feat: Adding spline class to library

--- setup.py
+++ setup.py
@@ -1,14 +1,31 @@
-from distutils.core import setup
+import pathlib
+from setuptools import setup, find_packages
+import sys
+
+# Python supported version checks
+if sys.version_info[:2] < (3, 7):
+    raise RuntimeError("Python version >= 3.7 required.")
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
 
 setup(
-    name = 'my_python_package',
-    packages = ['my_python_package'],
+    name = 'BSplineFit',
     version = '1.0.0',  # Ideally should be same as your GitHub release tag varsion
     description = 'A uniform cubic B-spline regression library',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url = 'https://github.com/MarceloJacinto/BSplineFit',
     author = 'MarceloJacinto',
     author_email = 'marcelo.jacinto@tecnico.ulisboa.pt',
-    url = 'https://github.com/MarceloJacinto/BSplineFit',
-    download_url = 'https://github.com/MarceloJacinto/BSplineFit',
-    keywords = ['BSpline', 'CubicBSplineFit'],
+    license="MIT",
     classifiers = [],
+    packages = find_packages(exclude=("tests",)),
+    include_package_data=True,
+    platforms = ["Windows", "Linux", "Solaris", "Mac OS-X", "Unix"],
+    python_requires='>=3.8',
+    install_requires=["numpy", "scipy", "sklearn"],
 )
